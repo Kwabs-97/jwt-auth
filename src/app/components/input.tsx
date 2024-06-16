3a7ae5b14425589c45822f3bbc1bffaeace237e7
Kwabs-github <107767920+Kwabs-97@users.noreply.github.com>
2024-06-16 01:15:43 +0000

    login form done

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 interface InputProps {
   type: string;
-  value: string | number;
-  onChange: (event: React.ChangeEvent<HTMLElement>) => void;
-  className: string;
   placeholder: string;
+  name: string;
+  className: string;
 }
 
-function Input({ type, value, onChange, className, placeholder }: InputProps) {
+function Input({ type, className, placeholder, name }: InputProps) {
   return (
     <input
       type={type}
-      value={value}
-      className={`${className} `}
-      onChange={onChange}
+      className={`${className} py-2  `}
       placeholder={placeholder}
+      name={name}
     />
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
-import Image from "next/image";
+import { useForm, SubmitHandler } from "react-hook-form";
+import Input from "./components/input";
+import Button from "./components/button";
 
 export default function Home() {
-  return <></>;
+  const { register, handleSubmit, formState, watch } = useForm();
+  return (
+    <div>
+      <form>
+        <div id="email-input">
+          <label htmlFor="email">Email</label>
+          <Input
+            type="email"
+            placeholder="please enter your email"
+            className=" text-base text-black leading-3"
+            {...register("email")}
+          />
+        </div>
+        <div id="password-input">
+          <label htmlFor="password">Password</label>
+          <Input
+            type="password"
+            placeholder="please enter your email"
+            className=" text-base text-black leading-3"
+            {...register("password")}
+          />
+        </div>
+        <Button className="">sign in</Button>
+      </form>
+    </div>
+  );
 }
